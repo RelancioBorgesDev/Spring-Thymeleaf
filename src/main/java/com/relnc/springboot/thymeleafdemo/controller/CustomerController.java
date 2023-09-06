84c1b8aae2993c70f7a7be5dcdc1da0cd173cd85
Relancio Borges <relancio.dev@gmail.com>
2023-09-06 12:19:57 -0300

    feat(controller): adicionado método processForm

--- src/main/java/com/relnc/springboot/thymeleafdemo/controller/CustomerController.java
+++ src/main/java/com/relnc/springboot/thymeleafdemo/controller/CustomerController.java
@@ -1,9 +1,13 @@
 package com.relnc.springboot.thymeleafdemo.controller;
 
 import com.relnc.springboot.thymeleafdemo.model.Customer;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
@@ -12,5 +16,15 @@ public class CustomerController {
         theModel.addAttribute("customer", new Customer());
 
         return "customer-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,
+                              BindingResult theBindingResult){
+            if(theBindingResult.hasErrors()) {
+                return "customer-form";
+            }else {
+                return "customer-confirmation";
+            }
     }
 }
